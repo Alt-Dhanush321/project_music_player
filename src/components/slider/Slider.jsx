0a8fbe80bd13ea0c92ec8a41cd1c43c6aad2da1d
Alt-Dhanush321 <dhanush321alt@gmail.com>
2023-08-30 13:18:39 +0530

    new card

--- src/components/slider/Slider.jsx
+++ src/components/slider/Slider.jsx
@@ -1,46 +1,39 @@
-import React, { useState } from 'react'
-import { FaArrowAltCircleRight, FaArrowAltCircleLeft } from 'react-icons/fa';
-import {images} from './const'
-import './Slider.css'
+// import React, {useState} from 'https://cdn.skypack.dev/react';
+// import ReactDOM from 'https://cdn.skypack.dev/react-dom';
 
-function Slider() {
+import React, {useState} from "https://esm.sh/react@18";
+import ReactDOM from "https://esm.sh/react-dom@18";
 
-    const [current , setCurrent] = useState(0);
-    const slideLen = images.length;
+import {TiChevronLeftOutline, TiChevronRightOutline} from 'https://cdn.skypack.dev/react-icons/ti';
 
-    
-    const nextSlide = () => {
-        setCurrent(current === slideLen - 1 ? 0 : current + 1);
-    };
+const MAX_VISIBILITY = 3;
 
-    const prevSlide = () => {
-        setCurrent(current === 0 ? slideLen - 1 : current - 1);
-    };  
 
-    
 
+const Carousel = ({children}) => {
+  const [active, setActive] = useState(2);
+  const count = React.Children.count(children);
+  
   return (
-    <>
-        <section className="slider-container w-full h-24">
-            <div className="nav-arrows flex w-full absolute justify-between px-32 text-white text-4xl top-1/4">
-                <FaArrowAltCircleLeft className='left-arrow' onClick={prevSlide} />
-                <FaArrowAltCircleRight className='right-arrow' onClick={nextSlide} />
-            </div>
-            {
-                images.map((slide,i)=>(
-                    <div className={i === current ? 'slide active' : 'slide'} key={i}>
-                         {
-                            i === current && (
-                            <img src={slide.ImgSrc} alt='travel image' className='slide-image object-cover w-full h-[500px] ' />
-                            )
-                        }
-                    </div>
-                ))
-            }
+    <div className='carousel'>
+      {active > 0 && <button className='nav left' onClick={() => setActive(i => i - 1)}><TiChevronLeftOutline/></button>}
+      {React.Children.map(children, (child, i) => (
+        <div className='card-container' style={{
+            '--active': i === active ? 1 : 0,
+            '--offset': (active - i) / 3,
+            '--direction': Math.sign(active - i),
+            '--abs-offset': Math.abs(active - i) / 3,
+            'pointer-events': active === i ? 'auto' : 'none',
+            'opacity': Math.abs(active - i) >= MAX_VISIBILITY ? '0' : '1',
+            'display': Math.abs(active - i) > MAX_VISIBILITY ? 'none' : 'block',
+          }}>
+          {child}
+        </div>
+      ))}
+      {active < count - 1 && <button className='nav right' onClick={() => setActive(i => i + 1)}><TiChevronRightOutline/></button>}
+    </div>
+  );
+};
 
-        </section>
-    </>
-  )
-}
 
-export default Slider
+export default Carousel;
